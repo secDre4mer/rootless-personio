a20d8392664aa1f3910582bc6f8c0dc750fffbd9
Kalle Fagerberg <kalle.fagerberg@riskident.com>
2023-03-30 13:04:31 +0200

    Fixed "CSRF token not found" issue.
    This removes the CSRF token regex from the HTML, and instead relies on
    the CSRF token cookie that Personio now sends.
    
    This resolves #6

--- pkg/personio/auth.go
+++ pkg/personio/auth.go
@@ -26,13 +26,10 @@ import (
 	"net/http"
 	"net/url"
 	"regexp"
-	"strconv"
 	"strings"
 )
 
 var (
-	employeeIDRegex     = regexp.MustCompile(`window.EMPLOYEE\s*=\s*{\s*id:\s*(\d+),`)
-	csrfTokenRegex      = regexp.MustCompile(`name="_token"[^>]*(?:value|content)="([^"]*)"`)
 	csrfTokenErrorRegex = regexp.MustCompile(`REDUX_INITIAL_STATE\.bladeState\.messages\s*=\s*{[^}]*error:\s*"((?:\\"|[^"])*)"`)
 )
 
@@ -86,41 +83,72 @@ func (c *Client) Login(email, pass string) error {
 		return err
 	}
 
-	resp, err := c.RawForm(req)
-	if err != nil {
+	if _, err := c.RawForm(req); err != nil {
 		return err
 	}
 
-	body, err := ioutil.ReadAll(resp.Body)
-	resp.Body.Close()
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	tokenMatch := csrfTokenRegex.FindSubmatch(body)
-	if tokenMatch == nil {
-		return ErrCSRFTokenNotFound
-	}
-	c.csrfToken = string(tokenMatch[1])
-
-	idMatch := employeeIDRegex.FindSubmatch(body)
-	if idMatch == nil {
-		if strings.HasSuffix(resp.Request.URL.Path, "/login/token-auth") {
-			return LockedAccountError{
-				CSRFToken: string(tokenMatch[1]),
-				Response:  resp,
-			}
-		}
-		return ErrEmployeeIDNotFound
-	}
-
-	id, err := strconv.Atoi(string(idMatch[1]))
+	userActivity, err := c.getUserActivity()
 	if err != nil {
 		return fmt.Errorf("%w: %s", ErrEmployeeIDNotFound, err)
 	}
-
-	c.EmployeeID = id
+	c.EmployeeID = userActivity.Visitor.ID
 	return nil
+}
+
+// getUserActivity seems to get info about the currently logged in user.
+//
+// Don't know for certain what this endpoint is, so keeping the function as
+// private in the meantime.
+func (c *Client) getUserActivity() (*userActivity, error) {
+	req, err := http.NewRequest(http.MethodGet, "/user-activity/api/v1/pendo", nil)
+	if err != nil {
+		return nil, fmt.Errorf("create request: %w", err)
+	}
+	resp, err := c.RawJSON(req)
+	if err != nil {
+		return nil, err
+	}
+	return ParseResponseJSON[*userActivity](resp)
+}
+
+type userActivity struct {
+	Account userActivityAccount `json:"account"`
+	Enabled bool                `json:"enabled"`
+	Visitor userActivityVisitor `json:"visitor"`
+}
+
+type userActivityVisitor struct {
+	EmploymentType      string `json:"employment_type"`        // ex: "internal"
+	HasReports          bool   `json:"has_reports"`            // ex: false
+	HasSupervisor       bool   `json:"has_supervisor"`         // ex: true
+	ID                  int    `json:"id"`                     // ex: 8234095
+	Role                string `json:"role"`                   // ex: "Employee"
+	Status              string `json:"status"`                 // ex: "active"
+	UserCreatedAt       string `json:"user_created_at"`        // ex: "2006-01-02 15:04:05"
+	UserIsAccountOwner  bool   `json:"user_is_account_owner"`  // ex: false
+	UserIsAdmin         bool   `json:"user_is_admin"`          // ex: false
+	UserIsContractOwner bool   `json:"user_is_contract_owner"` // ex: false
+	UserLang            string `json:"user_lang"`              // ex: "en"
+}
+
+type userActivityAccount struct {
+	AccountActiveEmployees     int    `json:"account_active_employees"`     // ex: 123
+	AccountCreatedAt           string `json:"account_created_at"`           // ex: "2006-01-02 15:04:05"
+	AccountExpiresOn           string `json:"account_expires_on"`           // ex: "2006-01-02"
+	AccountHostname            string `json:"account_hostname"`             // ex: "mycompany"
+	AccountIsTest              bool   `json:"account_is_test"`              // ex: false
+	AccountName                string `json:"account_name"`                 // ex: "My Company GmbH"
+	AccountPlanID              int    `json:"account_plan_id"`              // ex: 123
+	AccountSubcompaniesEnabled bool   `json:"account_subcompanies_enabled"` // ex: false
+	Addons                     string `json:"addons"`                       // ex: "productivity_plus,customization_plus,automation_plus"
+	BillingCountry             string `json:"billing_country"`              // ex: "DE"
+	EnabledPayrollIntegration  any    `json:"enabled_payroll_integration"`  // ex: null
+	ID                         int    `json:"id"`                           // ex: 1234
+	IsTrialAccount             bool   `json:"is_trial_account"`             // ex: false
+	PlanStatus                 string `json:"plan_status"`                  // ex: "active"
+	PlanType                   string `json:"plan_type"`                    // ex: "professional"
+	PlanVersion                int    `json:"plan_version"`                 // ex: 5
+	TrialConversionDate        any    `json:"trial_conversion_date"`        // ex: null
 }
 
 type LockedAccountError struct {
